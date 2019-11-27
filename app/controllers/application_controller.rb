6c39c3871c3041daec129ac2b7e9ef5cecc75157
Austin Asbury <acasbury25@aol.com>
2019-11-27 18:39:42 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,9 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  delete '/recipes/:id' do #destroy action
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
 end
